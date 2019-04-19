4c5210809a644678af7c3cb2f84fbb4b3d2f4836
akhileshkr797 <akhileshkr797@gmail.com>
2019-04-19 19:26:42 +0530

    added events

--- main.js
+++ main.js
@@ -3,11 +3,41 @@ const { app, BrowserWindow, webContents, ipcMain } = electron
 const path = require('path')
 const url = require('url')
 
-let mainWindow
+let mainWindow, secondWindow
 
 function createWindow(fileStr, options) {
     let win = new BrowserWindow(options)
+        //webcontents events
 
+    win.webContents.on('did-start-loading', event => {
+        console.log('did-start-loading', event.sender.webContents.browserWindowOptions.title)
+    })
+
+    win.webContents.on('dom-ready', event => {
+        console.log('dom-ready')
+    })
+
+    win.webContents.on('did-finish-load', event => {
+        console.log('did-finish-load', event.sender.webContents.getTitle())
+    })
+
+    win.webContents.on('did-stop-loading', event => {
+        console.log('did-stop-loading', event.sender.webContents.id)
+    })
+
+    win.webContents.on('did-fail-load', event => {
+        console.log('did-fail-load', event.sender.webContents.getTitle())
+    })
+
+    win.webContents.on('did-frame-finish-load', event => {
+        console.log('did-frame-finish-load', event.sender.webContents.getTitle())
+    })
+
+    win.webContents.on('did-get-response-details', event => {
+        console.log('did-get-response-details', event.sender.webContents.browserWindowOptions.id)
+    })
+
+    //loadinf files
     win.loadURL(url.format({
         pathname: path.join(__dirname, fileStr),
         protocol: 'file:',
@@ -23,6 +53,16 @@ function createWindow(fileStr, options) {
     })
 
     return win
+
+    //webcontent Events
+
+    win.webContents.on('did-start-loading', event => {
+        console.log('did-start-loading:', event.sender.webContents.browserWindowOptions.title)
+    })
+
+    win.webContents.on('dom-ready', event => {
+        console.log('dom-ready')
+    })
 }
 
 app.on('ready', event => {
@@ -32,5 +72,12 @@ app.on('ready', event => {
         height: 800,
         title: 'mainWiandow',
         backgroundColor: '#ece'
+    })
+
+    secondWindow = createWindow('about.html', {
+        width: 800,
+        height: 500,
+        title: 'secondWindow',
+        backgroundColor: '#dcd'
     })
 })
